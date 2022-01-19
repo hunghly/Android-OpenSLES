a292c9d5b502d1ddc3e1957d5319fcd04bf2484d
Hung Ly <hunghly@gmail.com>
2022-01-19 16:06:23 -0600

    refactored create_engine

--- src/test-audio.cpp
+++ src/test-audio.cpp
@@ -9,9 +9,72 @@
 // #include <SLES/OpenSLES_AndroidConfiguration.h>
 // #include <SLES/OpenSLES_AndroidMetadata.h>
 
+// signatures
+void create_engine();
+
+// engine interfaces
+static SLEngineItf engineEngine; // This interface exposes creation methods of all the OpenSL ES object types.
+static SLObjectItf engineObject = NULL; //The SLObjectItf interface provides essential utility methods for all objects. 
+
+// output mix interfaces
+static SLObjectItf outputMixObject = NULL;
+static SLEnvironmentalReverbItf outputMixEnvironmentalReverb = NULL;
+
+// aux effect on the output mix, used by the buffer queue player
+static const SLEnvironmentalReverbSettings reverbSettings =
+    SL_I3DL2_ENVIRONMENT_PRESET_ROOM;
+
 int main()
 {
     LOGD("Starting Audio Binary");
     std::cout << "Hello World!!" << std::endl;
+    std::cout << "Creating Audio Engine" << std::endl;
+    create_engine();
     return 0;
+}
+
+void create_engine()
+{
+    SLresult result;
+
+    // create engine
+    result = slCreateEngine(&engineObject, 0, NULL, 0, NULL, NULL);
+    assert(SL_RESULT_SUCCESS == result);
+    (void)result;
+
+    // realize the engine
+    result = (*engineObject)->Realize(engineObject, SL_BOOLEAN_FALSE);
+    assert(SL_RESULT_SUCCESS == result);
+    (void)result;
+
+    // get the engine interface, which is needed in order to create other objects
+    result = (*engineObject)->GetInterface(engineObject, SL_IID_ENGINE, &engineEngine);
+    assert(SL_RESULT_SUCCESS == result);
+    (void)result;
+
+    // create output mix, with environmental reverb specified as a non-required interface
+    const SLInterfaceID ids[1] = {SL_IID_ENVIRONMENTALREVERB};
+    const SLboolean req[1] = {SL_BOOLEAN_FALSE};
+    result = (*engineEngine)->CreateOutputMix(engineEngine, &outputMixObject, 1, ids, req);
+    assert(SL_RESULT_SUCCESS == result);
+    (void)result;
+
+    // realize the output mix
+    result = (*outputMixObject)->Realize(outputMixObject, SL_BOOLEAN_FALSE);
+    assert(SL_RESULT_SUCCESS == result);
+    (void)result;
+
+    // get the environmental reverb interface
+    // this could fail if the environmental reverb effect is not available,
+    // either because the feature is not present, excessive CPU load, or
+    // the required MODIFY_AUDIO_SETTINGS permission was not requested and granted
+    result = (*outputMixObject)->GetInterface(outputMixObject, SL_IID_ENVIRONMENTALREVERB,
+            &outputMixEnvironmentalReverb);
+    if (SL_RESULT_SUCCESS == result) {
+        result = (*outputMixEnvironmentalReverb)->SetEnvironmentalReverbProperties(
+                outputMixEnvironmentalReverb, &reverbSettings);
+        (void)result;
+    }
+    // ignore unsuccessful result codes for environmental reverb, as it is optional for this example
+    std::cout << "No Issues" << std::endl;
 }
