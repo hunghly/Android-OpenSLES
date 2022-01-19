b596e6437f297e93a39b9b33d6938f06a5939ec4
Hung Ly <hunghly@gmail.com>
2022-01-19 17:11:07 -0600

    need to test recording feature

--- jni/Application.mk
+++ jni/Application.mk
@@ -3,4 +3,4 @@ APP_ABI := x86 # Define the target architecture to be ARM.
 APP_STL := c++_static
 APP_CPPFLAGS := -frtti -fexceptions \
 -std=c++11   # This is the place you enable exception.
-APP_PLATFORM := android-16 # Define the target Android version of the native application.
+APP_PLATFORM := android-22 # Define the target Android version of the native application.
--- src/test-audio.cpp
+++ src/test-audio.cpp
@@ -11,6 +11,17 @@
 
 // signatures
 void create_engine();
+void create_recorder();
+void bqRecorderCallback(SLAndroidSimpleBufferQueueItf bq, void *context);
+void set_recording_state();
+
+// a mutext to guard against re-entrance to record & playback
+// as well as make recording and playing back to be mutually exclusive
+// this is to avoid crash at situations like:
+//    recording is in session [not finished]
+//    user presses record button and another recording coming in
+// The action: when recording/playing back is not finished, ignore the new request
+static pthread_mutex_t  audioEngineLock = PTHREAD_MUTEX_INITIALIZER;
 
 // engine interfaces
 static SLEngineItf engineEngine; // This interface exposes creation methods of all the OpenSL ES object types.
@@ -20,16 +31,40 @@ static SLObjectItf engineObject = NULL; //The SLObjectItf interface provides ess
 static SLObjectItf outputMixObject = NULL;
 static SLEnvironmentalReverbItf outputMixEnvironmentalReverb = NULL;
 
+// recorder interfaces
+static SLObjectItf recorderObject = NULL;
+static SLRecordItf recorderRecord;
+static SLAndroidSimpleBufferQueueItf recorderBufferQueue;
+
 // aux effect on the output mix, used by the buffer queue player
 static const SLEnvironmentalReverbSettings reverbSettings =
     SL_I3DL2_ENVIRONMENT_PRESET_ROOM;
+
+// 5 seconds of recorded audio at 16 kHz mono, 16-bit signed little endian
+#define RECORDER_FRAMES (16000 * 5)
+static short recorderBuffer[RECORDER_FRAMES];
+static unsigned recorderSize = 0;
 
 int main()
 {
     LOGD("Starting Audio Binary");
     std::cout << "Hello World!!" << std::endl;
     std::cout << "Creating Audio Engine" << std::endl;
+
     create_engine();
+    create_recorder();
+    set_recording_state();
+    
+    // clean up
+    // destroy audio recorder object, and invalidate all associated interfaces
+    if (recorderObject != NULL) {
+        std::cout << "Cleaning up recorderObject" << std::endl;
+        (*recorderObject)->Destroy(recorderObject);
+        recorderObject = NULL;
+        recorderRecord = NULL;
+        recorderBufferQueue = NULL;
+    }
+
     return 0;
 }
 
@@ -77,4 +112,115 @@ void create_engine()
     }
     // ignore unsuccessful result codes for environmental reverb, as it is optional for this example
     std::cout << "No Issues" << std::endl;
+}
+
+// create audio recorder: recorder is not in fast path
+//    like to avoid excessive re-sampling while playing back from Hello & Android clip
+void create_recorder()
+{
+    SLresult result;
+
+    // configure audio source
+    SLDataLocator_IODevice loc_dev = {SL_DATALOCATOR_IODEVICE, SL_IODEVICE_AUDIOINPUT,
+            SL_DEFAULTDEVICEID_AUDIOINPUT, NULL};
+    SLDataSource audioSrc = {&loc_dev, NULL};
+
+    // configure audio sink
+    SLDataLocator_AndroidSimpleBufferQueue loc_bq = {SL_DATALOCATOR_ANDROIDSIMPLEBUFFERQUEUE, 2};
+    SLDataFormat_PCM format_pcm = {SL_DATAFORMAT_PCM, 1, SL_SAMPLINGRATE_16,
+        SL_PCMSAMPLEFORMAT_FIXED_16, SL_PCMSAMPLEFORMAT_FIXED_16,
+        SL_SPEAKER_FRONT_CENTER, SL_BYTEORDER_LITTLEENDIAN};
+    SLDataSink audioSnk = {&loc_bq, &format_pcm};
+
+    // create audio recorder
+    // (requires the RECORD_AUDIO permission)
+    const SLInterfaceID id[1] = {SL_IID_ANDROIDSIMPLEBUFFERQUEUE};
+    const SLboolean req[1] = {SL_BOOLEAN_TRUE};
+    result = (*engineEngine)->CreateAudioRecorder(engineEngine, &recorderObject, &audioSrc,
+            &audioSnk, 1, id, req);
+    if (SL_RESULT_SUCCESS != result) {
+        std::cout << "Error CREATING audio recorder" << std::endl;
+    }
+
+    // realize the audio recorder
+    result = (*recorderObject)->Realize(recorderObject, SL_BOOLEAN_FALSE);
+    if (SL_RESULT_SUCCESS != result) {
+        std::cout << "Error REALIZING audio recorder" << std::endl;
+    }
+
+    // get the record interface
+    result = (*recorderObject)->GetInterface(recorderObject, SL_IID_RECORD, &recorderRecord);
+    assert(SL_RESULT_SUCCESS == result);
+    (void)result;
+
+    // get the buffer queue interface
+    result = (*recorderObject)->GetInterface(recorderObject, SL_IID_ANDROIDSIMPLEBUFFERQUEUE,
+            &recorderBufferQueue);
+    assert(SL_RESULT_SUCCESS == result);
+    (void)result;
+
+    std::cout << "Registering Callback" << std::endl;
+    // register callback on the buffer queue
+    result = (*recorderBufferQueue)->RegisterCallback(recorderBufferQueue, bqRecorderCallback,
+            NULL);
+    assert(SL_RESULT_SUCCESS == result);
+    (void)result;
+}
+
+// this callback handler is called every time a buffer finishes recording
+void bqRecorderCallback(SLAndroidSimpleBufferQueueItf bq, void *context)
+{
+    std::cout << "Inside of register callback" << std::endl;
+    LOGD("Recording Callback Triggered");
+    assert(bq == recorderBufferQueue);
+    assert(NULL == context);
+    // for streaming recording, here we would call Enqueue to give recorder the next buffer to fill
+    // but instead, this is a one-time buffer so we stop recording
+    SLresult result;
+    result = (*recorderRecord)->SetRecordState(recorderRecord, SL_RECORDSTATE_STOPPED);
+    if (SL_RESULT_SUCCESS == result) {
+        recorderSize = RECORDER_FRAMES * sizeof(short);
+    }
+    pthread_mutex_unlock(&audioEngineLock);
+}
+
+// set the recording state for the audio recorder
+void set_recording_state()
+{
+    SLresult result;
+
+    if (pthread_mutex_trylock(&audioEngineLock)) {
+        std::cout << "Unable to unlock mutex lock" << std::endl;
+        return;
+    }
+    // in case already recording, stop recording and clear buffer queue
+    result = (*recorderRecord)->SetRecordState(recorderRecord, SL_RECORDSTATE_STOPPED);
+    assert(SL_RESULT_SUCCESS == result);
+    (void)result;
+    result = (*recorderBufferQueue)->Clear(recorderBufferQueue);
+    assert(SL_RESULT_SUCCESS == result);
+    (void)result;
+
+    // the buffer is not valid for playback yet
+    recorderSize = 0;
+
+    // enqueue an empty buffer to be filled by the recorder
+    // (for streaming recording, we would enqueue at least 2 empty buffers to start things off)
+    result = (*recorderBufferQueue)->Enqueue(recorderBufferQueue, recorderBuffer,
+            RECORDER_FRAMES * sizeof(short));
+    // the most likely other result is SL_RESULT_BUFFER_INSUFFICIENT,
+    // which for this code example would indicate a programming error
+    assert(SL_RESULT_SUCCESS == result);
+    (void)result;
+
+    // Set the duration of the recording - 30 seconds (30,000 milliseconds)
+    std::cout << "Setting duration to 5 seconds" << std::endl;
+    result = (*recorderRecord)->SetDurationLimit(recorderRecord, 5000);
+    assert(SL_RESULT_SUCCESS == result);
+
+    std::cout << "Starting recording..." << std::endl;
+    // start recording
+    result = (*recorderRecord)->SetRecordState(recorderRecord, SL_RECORDSTATE_RECORDING);
+    assert(SL_RESULT_SUCCESS == result);
+    (void)result;
 }
